83fe9b702e3ebc77616f3be162a9b393325c3f64
supratik110 <13682744+supratik110@users.noreply.github.com>
2019-01-22 22:38:08 +0530

    update

--- utill/commonUtill.groovy
+++ utill/commonUtill.groovy
@@ -2,8 +2,7 @@ pipeline {
 	agent any
         	stages {
                        script {
-								prop = readProperties file: 'Properties/pipelineFile.properties'
-								bat prop.javaConfigUrl
+								prop = readProperties file:'properties/common.properties'
 								configProp = load prop.configFile
 								configProp.loadProps()
 								configProp.readScanBuild()
