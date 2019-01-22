ced459a10951cdc0bd04348d4c6ebaa9e6fdce3c
supratik110 <13682744+supratik110@users.noreply.github.com>
2019-01-22 22:47:56 +0530

    fix

--- utill/commonUtill.groovy
+++ utill/commonUtill.groovy
@@ -1,9 +1,11 @@
 pipeline {
 	agent any
         	stages {
-                       
+                       stage('PRE LOAD') {
+                  	steps {
 			prop = readProperties file:'properties/common.properties'
 			configProp = load prop.configFile
+			}
 			configProp.loadProps()
 			configProp.readScanBuild()
 			configProp.artifactory()
