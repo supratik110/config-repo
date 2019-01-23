ca54e5f52d445afd4525fcd9d4def34a96a0ac45
supratik110 <13682744+supratik110@users.noreply.github.com>
2019-01-23 12:57:56 +0530

    mail

--- config/java-ci-cd.groovy
+++ config/java-ci-cd.groovy
@@ -27,7 +27,8 @@ def notifyBuild(String buildStatus)
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+    <p>Please give input to deploy"""
 	
  emailext (
       subject: subject,
