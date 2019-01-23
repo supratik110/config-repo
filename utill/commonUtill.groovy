f797ad7c07390020b68f5802fc922ca3977535c2
supratik110 <13682744+supratik110@users.noreply.github.com>
2019-01-23 12:12:15 +0530

    fix

--- utill/commonUtill.groovy
+++ utill/commonUtill.groovy
@@ -1,22 +1,17 @@
 pipeline {
 	agent any
         	stages {
-			stage('LOAD PROPERTIES FILES') {
+		stage('LOAD PROPERTIES FILES') {
                   steps {
                        script {
 		       	commonProps = readProperties file:'properties/common.properties'
-			echo 'LOAD SUCCESS1'
 			gitProps = readProperties file:'properties/git.properties'
-			echo 'LOAD SUCCESS2'
 			deployProps = readProperties file:'properties/deploy.properties'
-			echo 'LOAD SUCCESS3'
 			artifactoryProps = readProperties file:'properties/artifactory.properties'
-			echo 'LOAD SUCCESS4'
 			configProp = load commonProps.configFile
-			echo 'LOAD SUCCESS5'
-								}
-							}
-						}
+				}
+			}
+			}
 			stage('READ GIT') {
                   steps {
 		  	script {
