3744b0bdeb9db742d3ead009703a25af1bc0345f
supratik110 <13682744+supratik110@users.noreply.github.com>
2019-01-23 12:44:39 +0530

    input

--- utill/commonUtill.groovy
+++ utill/commonUtill.groovy
@@ -36,6 +36,15 @@ pipeline {
 			}
 				}
 			}
+			stage("foo") {
+            	steps {
+                	script {
+                    	env.RELEASE_SCOPE = input message: 'User input required', ok: 'Release!',
+                            parameters: [choice(name: 'RELEASE_SCOPE', choices: 'patch\nminor\nmajor', description: 'What is the release scope?')]
+                		}
+                	echo "${env.RELEASE_SCOPE}"
+            			}
+        		}
 			stage('UPLOAD ARTIFACT') {
                   steps {
 		  	script {
